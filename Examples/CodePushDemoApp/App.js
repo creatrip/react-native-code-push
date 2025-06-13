b7419cdab0643c6e01b91b9d18bd819409792321
creatrip-seungmin <seungmin.lee@creatrip.com>
2025-06-13 15:52:22 +0900

    feat: typescript ignore

--- Examples/CodePushDemoApp/App.js
+++ Examples/CodePushDemoApp/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   Dimensions,
@@ -9,45 +9,54 @@ import {
   View,
 } from 'react-native';
 
-import CodePush from "react-native-code-push";
+import CodePush from 'react-native-code-push';
 
 class App extends Component<{}> {
   constructor() {
     super();
-    this.state = { restartAllowed: true };
+    this.state = {restartAllowed: true};
   }
 
   codePushStatusDidChange(syncStatus) {
-    switch(syncStatus) {
+    switch (syncStatus) {
       case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
-        this.setState({ syncMessage: "Checking for update." });
+        this.setState({syncMessage: 'Checking for update.'});
         break;
       case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
-        this.setState({ syncMessage: "Downloading package." });
+        this.setState({syncMessage: 'Downloading package.'});
         break;
       case CodePush.SyncStatus.AWAITING_USER_ACTION:
-        this.setState({ syncMessage: "Awaiting user action." });
+        this.setState({syncMessage: 'Awaiting user action.'});
         break;
       case CodePush.SyncStatus.INSTALLING_UPDATE:
-        this.setState({ syncMessage: "Installing update." });
+        this.setState({syncMessage: 'Installing update.'});
         break;
       case CodePush.SyncStatus.UP_TO_DATE:
-        this.setState({ syncMessage: "App up to date.", progress: false });
+        this.setState({syncMessage: 'App up to date.', progress: false});
         break;
       case CodePush.SyncStatus.UPDATE_IGNORED:
-        this.setState({ syncMessage: "Update cancelled by user.", progress: false });
+        this.setState({
+          syncMessage: 'Update cancelled by user.',
+          progress: false,
+        });
         break;
       case CodePush.SyncStatus.UPDATE_INSTALLED:
-        this.setState({ syncMessage: "Update installed and will be applied on restart.", progress: false });
+        this.setState({
+          syncMessage: 'Update installed and will be applied on restart.',
+          progress: false,
+        });
         break;
       case CodePush.SyncStatus.UNKNOWN_ERROR:
-        this.setState({ syncMessage: "An unknown error occurred.", progress: false });
+        this.setState({
+          syncMessage: 'An unknown error occurred.',
+          progress: false,
+        });
         break;
     }
   }
 
   codePushDownloadDidProgress(progress) {
-    this.setState({ progress });
+    this.setState({progress});
   }
 
   toggleAllowRestart() {
@@ -55,16 +64,23 @@ class App extends Component<{}> {
       ? CodePush.disallowRestart()
       : CodePush.allowRestart();
 
-    this.setState({ restartAllowed: !this.state.restartAllowed });
+    this.setState({restartAllowed: !this.state.restartAllowed});
   }
 
   getUpdateMetadata() {
-    CodePush.getUpdateMetadata(CodePush.UpdateState.RUNNING)
-      .then((metadata: LocalPackage) => {
-        this.setState({ syncMessage: metadata ? JSON.stringify(metadata) : "Running binary version", progress: false });
-      }, (error: any) => {
-        this.setState({ syncMessage: "Error: " + error, progress: false });
-      });
+    CodePush.getUpdateMetadata(CodePush.UpdateState.RUNNING).then(
+      (metadata: LocalPackage) => {
+        this.setState({
+          syncMessage: metadata
+            ? JSON.stringify(metadata)
+            : 'Running binary version',
+          progress: false,
+        });
+      },
+      (error: any) => {
+        this.setState({syncMessage: 'Error: ' + error, progress: false});
+      },
+    );
   }
 
   /** Update is downloaded silently, and applied on restart (recommended) */
@@ -72,16 +88,16 @@ class App extends Component<{}> {
     CodePush.sync(
       {},
       this.codePushStatusDidChange.bind(this),
-      this.codePushDownloadDidProgress.bind(this)
+      this.codePushDownloadDidProgress.bind(this),
     );
   }
 
   /** Update pops a confirmation dialog, and then immediately reboots the app */
   syncImmediate() {
     CodePush.sync(
-      { installMode: CodePush.InstallMode.IMMEDIATE, updateDialog: true },
+      {installMode: CodePush.InstallMode.IMMEDIATE, updateDialog: true},
       this.codePushStatusDidChange.bind(this),
-      this.codePushDownloadDidProgress.bind(this)
+      this.codePushDownloadDidProgress.bind(this),
     );
   }
 
@@ -90,15 +106,16 @@ class App extends Component<{}> {
 
     if (this.state.progress) {
       progressView = (
-        <Text style={styles.messages}>{this.state.progress.receivedBytes} of {this.state.progress.totalBytes} bytes received</Text>
+        <Text style={styles.messages}>
+          {this.state.progress.receivedBytes} of{' '}
+          {this.state.progress.totalBytes} bytes received
+        </Text>
       );
     }
 
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to CodePush!
-        </Text>
+        <Text style={styles.welcome}>Welcome to CodePush!</Text>
         <TouchableOpacity onPress={this.sync.bind(this)}>
           <Text style={styles.syncButton}>Press for background sync</Text>
         </TouchableOpacity>
@@ -106,14 +123,20 @@ class App extends Component<{}> {
           <Text style={styles.syncButton}>Press for dialog-driven sync</Text>
         </TouchableOpacity>
         {progressView}
-        <Image style={styles.image} resizeMode={"contain"} source={require("./images/laptop_phone_howitworks.png")}/>
+        <Image
+          style={styles.image}
+          resizeMode={'contain'}
+          source={require('./images/laptop_phone_howitworks.png')}
+        />
         <TouchableOpacity onPress={this.toggleAllowRestart.bind(this)}>
-          <Text style={styles.restartToggleButton}>Restart { this.state.restartAllowed ? "allowed" : "forbidden"}</Text>
+          <Text style={styles.restartToggleButton}>
+            Restart {this.state.restartAllowed ? 'allowed' : 'forbidden'}
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={this.getUpdateMetadata.bind(this)}>
           <Text style={styles.syncButton}>Press for Update Metadata</Text>
         </TouchableOpacity>
-        <Text style={styles.messages}>{this.state.syncMessage || ""}</Text>
+        <Text style={styles.messages}>{this.state.syncMessage || ''}</Text>
       </View>
     );
   }
@@ -122,31 +145,31 @@ class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-    paddingTop: 50
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 50,
   },
   image: {
     margin: 30,
-    width: Dimensions.get("window").width - 100,
-    height: 365 * (Dimensions.get("window").width - 100) / 651,
+    width: Dimensions.get('window').width - 100,
+    height: (365 * (Dimensions.get('window').width - 100)) / 651,
   },
   messages: {
     marginTop: 30,
-    textAlign: "center",
+    textAlign: 'center',
   },
   restartToggleButton: {
-    color: "blue",
-    fontSize: 17
+    color: 'blue',
+    fontSize: 17,
   },
   syncButton: {
-    color: "green",
-    fontSize: 17
+    color: 'green',
+    fontSize: 17,
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 20
+    textAlign: 'center',
+    margin: 20,
   },
 });
 
@@ -155,8 +178,6 @@ const styles = StyleSheet.create({
  * different check frequency, such as ON_APP_START, for a 'hands-off' approach where CodePush.sync() does not
  * need to be explicitly called. All options of CodePush.sync() are also available in this decorator.
  */
-let codePushOptions = { checkFrequency: CodePush.CheckFrequency.MANUAL };
+let codePushOptions = {checkFrequency: CodePush.CheckFrequency.MANUAL};
 
-App = CodePush(codePushOptions)(App);
-
-export default App;
+export default CodePush(codePushOptions)(App);
